33b8a27441b430dc9539afb3b6a4856aa2c71b4e
Thejus Rajendran <thejusr1999@gmail.com>
2023-02-15 16:44:10 +0530

    added formik

--- app/createTask/page.tsx
+++ app/createTask/page.tsx
@@ -1,41 +1,89 @@
-export default function createTask() {
+"use client";
+
+import { fmtDateAndTime } from "@/utils/DateUtils";
+import { useFormik } from "formik";
+
+export default function CreateTask() {
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+      desc: "",
+      startTime: "",
+      startDate: "",
+      endTime: "",
+      endDate: "",
+    },
+    onSubmit: (values) => {
+      const start = fmtDateAndTime(values.startDate, values.startTime);
+      const end = fmtDateAndTime(values.endDate, values.endTime);
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
   return (
     <>
       Create New Task
-      <form>
+      <form onSubmit={formik.handleSubmit}>
         <div>
-          <label htmlFor="taskName"> Task Name </label>
+          <label htmlFor="title"> Title </label>
           <input
             type="text"
-            name="taskName"
-            id="taskName"
-            placeholder="enter task name"
+            name="title"
+            id="title"
+            onChange={formik.handleChange}
+            value={formik.values.title}
+            placeholder="Title"
           />
         </div>
         <div>
-          <label htmlFor="taskDesc"> Task Desc </label>
+          <label htmlFor="desc"> Description: </label>
           <input
             type="text"
-            name="taskDesc"
-            id="taskDesc"
-            placeholder="enter task Desc"
+            name="desc"
+            id="desc"
+            onChange={formik.handleChange}
+            value={formik.values.desc}
+            placeholder="More about the task"
           />
         </div>
         <div>
-          <label htmlFor="starting"> Task Name </label>
+          <label htmlFor="start"> Starting: </label>
           <input
             type="date"
-            name="taskName"
-            id="taskName"
-            placeholder="enter task name"
+            name="startDate"
+            id="startDate"
+            onChange={formik.handleChange}
+            value={formik.values.startDate}
+            placeholder="More about the task"
           />
           <input
             type="time"
-            name="taskName"
-            id="taskName"
-            placeholder="enter task name"
+            name="startTime"
+            id="startTime"
+            onChange={formik.handleChange}
+            value={formik.values.startTime}
+            placeholder="More about the task"
           />
         </div>
+        <div>
+          <label htmlFor="start"> Ending: </label>
+          <input
+            type="date"
+            name="endDate"
+            id="endDate"
+            onChange={formik.handleChange}
+            value={formik.values.startDate}
+            placeholder="More about the task"
+          />
+          <input
+            type="time"
+            name="endTime"
+            id="endTime"
+            onChange={formik.handleChange}
+            value={formik.values.startTime}
+            placeholder="More about the task"
+          />
+        </div>
+        <button type="submit">Create Task</button>
       </form>
     </>
   );
