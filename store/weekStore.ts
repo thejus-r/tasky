e1f6a3f8983fdd5addff7ead88ef88f66d7a764f
Thejus Rajendran <thejusr1999@gmail.com>
2023-03-07 01:30:08 +0530

    zustand based weekstore

---
+++ store/weekStore.ts
@@ -0,0 +1,19 @@
+import { getWeekIndex, weekMoment } from "@/utils/DateUtils";
+import { create } from "zustand";
+
+type State = {
+  today: Date;
+  active: Date;
+};
+
+type Action = {
+  setActive: (date: Date) => void;
+};
+
+const useWeekStore = create<State & Action>((set, get) => ({
+  today: new Date(),
+  active: new Date(),
+  setActive: (date) => set(() => ({ active: date })),
+}));
+
+export default useWeekStore;
