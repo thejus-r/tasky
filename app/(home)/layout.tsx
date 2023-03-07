a2627a83e77fb0f91082cdd5b3a7a33d4d80a1b3
Thejus Rajendran <thejusr1999@gmail.com>
2023-03-07 22:35:31 +0530

    layout changes

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,20 +1,21 @@
-import '../globals.css'
+import "../globals.css";
 
 export const metadata = {
-  title: 'Tasky',
-  description: 'Manage your tasks easily',
-}
+  title: "Tasky",
+  description: "Manage your tasks easily",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className='bg-stone-900 text-white'>
-        <p>This layout only for homepage</p>
-        {children}</body>
+      <body className="bg-stone-900 text-white p-4">
+        <h1 className="text-xl font-medium mb-4">Tasky</h1>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
