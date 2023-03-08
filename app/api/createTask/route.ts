33e3f30593e43c017927cbab4bc7c592e16c509b
Thejus Rajendran <thejusr1999@gmail.com>
2023-03-08 21:39:24 +0530

    api update

--- app/api/createTask/route.ts
+++ app/api/createTask/route.ts
@@ -1,15 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
+import { locale } from "@/config/locale";
 
 export async function POST(req: NextRequest) {
   const data = await req.formData();
+
+  let date = new Date(data.get("date") as string);
 
   try {
     const task = await prisma.task.create({
       data: {
         title: data.get("title") as string,
         description: data.get("description") as string,
-        date: new Date(data.get("date") as string),
+        date: date,
       },
     });
     console.log(task);
