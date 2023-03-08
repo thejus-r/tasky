e3ef38211516b80675e27e24cdc0923a81c1f803
Thejus Rajendran <thejusr1999@gmail.com>
2023-03-08 20:26:12 +0530

    api - create task

--- app/api/createTask/route.ts
+++ app/api/createTask/route.ts
@@ -1,7 +1,21 @@
 import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
 
 export async function POST(req: NextRequest) {
   const data = await req.formData();
-  console.log(data.get("title"));
-  return NextResponse.json({ name: "Thejus R" });
+
+  try {
+    const task = await prisma.task.create({
+      data: {
+        title: data.get("title") as string,
+        description: data.get("description") as string,
+        date: new Date(data.get("date") as string),
+      },
+    });
+    console.log(task);
+    return NextResponse.json({ status: "OK" });
+  } catch (err) {
+    console.log(err);
+  }
+  return NextResponse.json({ status: "Error" });
 }
