cf7f0457a1b98115129bc422657ea4b6a6142cc3
Thejus Rajendran <thejusr1999@gmail.com>
2023-03-09 13:11:57 +0530

    init

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div className="text-xl font-extrabold text-red-500">Hello World</div>
+    ),
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
