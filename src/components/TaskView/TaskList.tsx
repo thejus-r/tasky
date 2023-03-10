0732b211086e61c8ad4b4bd5a2e748ea3896a74a
Thejus Rajendran <thejusr1999@gmail.com>
2023-03-10 23:49:08 +0530

    added a empty state for the tasklist

--- src/components/TaskView/TaskList.tsx
+++ src/components/TaskView/TaskList.tsx
@@ -2,6 +2,16 @@ import { Task } from "../../types/task";
 import TaskCard from "./TaskCard";
 
 export default function TaskList(props: { tasks: Task[] }) {
+  if (props.tasks.length === 0) {
+    return (
+      <div className="flex gap-1 flex-col h-96 justify-center items-center">
+        <h3 className="text-lg font-semibold">Damn! It's looking empty!</h3>
+        <p className="text-base text-stone-400">
+          If you have a planned task make sure you add the task.
+        </p>
+      </div>
+    );
+  }
   return (
     <div>
       {props.tasks.map((task) => (
