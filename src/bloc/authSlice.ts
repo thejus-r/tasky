28818a2e43d63b3437440146dab3d4f761a8c7c8
Thejus Rajendran <thejusr1999@gmail.com>
2023-03-13 15:47:45 +0530

    initState to presist auth state

--- src/bloc/authSlice.ts
+++ src/bloc/authSlice.ts
@@ -1,16 +1,23 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { User } from "@supabase/supabase-js";
+import supabase from "../lib/database";
 
+supabase.auth.getUser();
 interface AuthState {
-  user: User | null;
+  user: null | User;
   isLogged: boolean;
   hasError: boolean;
   error: string;
 }
 
+async function initState() {
+  const res = await supabase.auth.getUser();
+  return res.data.user;
+}
+
 const initialState: AuthState = {
-  user: null,
-  isLogged: false,
+  user: await initState(),
+  isLogged: (await initState()) !== null,
   hasError: false,
   error: "",
 };
